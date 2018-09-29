9fd04f85be1fa37e7fcb4953e656359bb111f147
ijwddd <ijwddd@163.com>
2018-09-29 21:30:54 +0800

    第二天 添加和修改

--- pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -11,71 +11,95 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serializable;
 import java.util.List;
+
 /**
  * BrandServiceImpl 服务接口实现类
- * @date 2018-09-26 11:12:14
+ *
  * @version 1.0
+ * @date 2018-09-26 11:12:14
  */
 
 @Service(interfaceName = "com.pinyougou.service.BrandService")
 //@Transactional
 public class BrandServiceImpl implements BrandService {
 
-	@Autowired
-	private BrandMapper brandMapper;
+    @Autowired
+    private BrandMapper brandMapper;
 
-	/** 添加方法 */
-	public void save(Brand brand){
+    /**
+     * 添加品牌
+     */
+    public void save(Brand brand) {
+        try {
+            brandMapper.insertSelective(brand);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	}
+    /**
+     * 修改方法
+     */
+    public void update(Brand brand) {
+        try {
+            brandMapper.updateByPrimaryKeySelective(brand);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/** 修改方法 */
-	public void update(Brand brand){
+    /**
+     * 根据主键id删除
+     */
+    public void delete(Serializable id) {
 
-	}
+    }
 
-	/** 根据主键id删除 */
-	public void delete(Serializable id){
+    /**
+     * 批量删除
+     */
+    public void deleteAll(Serializable[] ids) {
 
-	}
+    }
 
-	/** 批量删除 */
-	public void deleteAll(Serializable[] ids){
+    /**
+     * 根据主键id查询
+     */
+    public Brand findOne(Serializable id) {
+        try {
+            return brandMapper.selectByPrimaryKey(id);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 
-	}
+    /**
+     * 查询全部
+     */
+    public List<Brand> findAll() {
+        try {
+            return brandMapper.selectAll();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 
-	/** 根据主键id查询 */
-	public Brand findOne(Serializable id){
-		try {
-			return brandMapper.selectByPrimaryKey(id);
-		}catch (Exception ex){
-			throw new RuntimeException(ex);
-		}
-	}
-
-	/** 查询全部 */
-	public List<Brand> findAll(){
-		try {
-			return brandMapper.selectAll();
-		}catch (Exception ex){
-			throw new RuntimeException(ex);
-		}
-	}
-
-	/** 多条件分页查询 */
-	public List<Brand> findByPage(Brand brand, int page, int rows){
-		try {
-			PageInfo<Brand> pageInfo = PageHelper.startPage(page, rows)
-				.doSelectPageInfo(new ISelect() {
-					@Override
-					public void doSelect() {
-						brandMapper.selectAll();
-					}
-				});
-			return pageInfo.getList();
-		}catch (Exception ex){
-			throw new RuntimeException(ex);
-		}
-	}
+    /**
+     * 多条件分页查询
+     */
+    public List<Brand> findByPage(Brand brand, int page, int rows) {
+        try {
+            PageInfo<Brand> pageInfo = PageHelper.startPage(page, rows)
+                    .doSelectPageInfo(new ISelect() {
+                        @Override
+                        public void doSelect() {
+                            brandMapper.selectAll();
+                        }
+                    });
+            return pageInfo.getList();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 
 }
