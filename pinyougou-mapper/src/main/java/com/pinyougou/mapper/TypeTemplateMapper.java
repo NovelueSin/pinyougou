737a24e6ebe38e74407d62ddf830b145a99c2ac9
ijwddd <ijwddd@163.com>
2018-10-10 17:14:10 +0800

    第四天 模板查询

--- pinyougou-mapper/src/main/java/com/pinyougou/mapper/TypeTemplateMapper.java
+++ pinyougou-mapper/src/main/java/com/pinyougou/mapper/TypeTemplateMapper.java
@@ -1,8 +1,9 @@
 package com.pinyougou.mapper;
 
+import com.pinyougou.pojo.TypeTemplate;
 import tk.mybatis.mapper.common.Mapper;
 
-import com.pinyougou.pojo.TypeTemplate;
+import java.util.List;
 
 /**
  * TypeTemplateMapper 数据访问接口
@@ -11,6 +12,5 @@ import com.pinyougou.pojo.TypeTemplate;
  */
 public interface TypeTemplateMapper extends Mapper<TypeTemplate>{
 
-
-
+    List<TypeTemplate> findAll(TypeTemplate typeTemplate);
 }
