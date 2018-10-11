eee12c7c00e385f2e3342fed12a5e4fbfc486162
ijwddd <ijwddd@163.com>
2018-10-11 21:35:00 +0800

    第四天 面包屑导航条

--- pinyougou-web/pinyougou-manager-web/src/main/webapp/js/controller/itemCatController.js
+++ pinyougou-web/pinyougou-manager-web/src/main/webapp/js/controller/itemCatController.js
@@ -58,4 +58,31 @@ app.controller('itemCatController', function($scope, $controller, baseService){
             alert("请选择要删除的记录！");
         }
     };
+
+    /*根据上级id显示下级列表*/
+    $scope.findItemCatByParentId = function (parentId) {
+        baseService.sendGet("/itemCat/findItemCatByParentId","parentId="+parentId).then(function (value) {
+            $scope.dataList = value.data;
+        })
+    }
+    
+    /*默认等级为1*/
+    $scope.grade = 1;
+    /*查询下级*/
+    $scope.selectList = function (entity, grade) {
+        $scope.grade = grade;
+        if(grade==1) {
+            $scope.itemCat_1 = null;
+            $scope.itemCat_2 = null;
+        }
+        if(grade==2) {
+            $scope.itemCat_1 = entity;
+            $scope.itemCat_2 = null;
+        }
+        if(grade==3) {
+            $scope.itemCat_2 = entity;
+        }
+      /*查询下级列表*/
+      $scope.findItemCatByParentId(entity.id);
+    };
 });
